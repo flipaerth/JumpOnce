de61f158700619652c66cb9e66e20b03cf198c67
flipaerth <austinozan@gmail.com>
2023-08-05 13:11:31 -0500

    Validated Jumping

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -71,34 +71,77 @@ public class PlayerController : MonoBehaviour
     // Reference for the RigidBody2D -> Drag the component into the reference to establish a line of communication
     public Rigidbody2D myRigidBody;
 
-    // Variables
+    // Player Variables
+    private float moveHorizontal;
+    private float moveSpeed = 10;
+
     public float jumpStrength = 7.5f;
-    public float speed = 10;
+
+    // Action Variables
+    public bool canJump; // Start with one jump
+    public bool canWallJump; // True when on the wall
+
+    public bool canDash; // Start with one dash
+    public bool canShoot; // Start with one bullet
+    public bool canDie; // Start with one life
+    public bool canGetHit; // Start with one hit
+
+    public bool isGrounded; // True when on the ground
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        canJump = true;
+        canWallJump = false;
+
+        canDash = false;
+        canShoot = false;
+        canDie = false;
+        canGetHit = false;
+
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Gets Unity's left and right horizontal input - Axis is a scale of -1 (left) to 1 (right)
-        float h = Input.GetAxis("Horizontal");
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
 
-        Rigidbody2D rb = myRigidBody;
+        // Creates The Vector Variable vel - Vector2 is an X and Y axis
         Vector2 vel = new Vector2();
 
-        vel.x = h * speed;
-        vel.y = rb.velocity.y;
+        // Horizontal Movement Speed - moving left or right
+        vel.x = moveHorizontal * moveSpeed;
+        // Vertical Movement Speed - like falling
+        vel.y = myRigidBody.velocity.y;
+        // Sets the velocities for the rigidbody
+        myRigidBody.velocity = vel;
 
-        rb.velocity = vel;
-
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        // Checks if the jump key is pressed, while also checking if the player can jump
+        if (Input.GetKeyDown(KeyCode.Space) == true && canJump == true && isGrounded == true)
         {
             myRigidBody.velocity = Vector2.up * jumpStrength;
-            Debug.Log("jump key pressed");
+            Debug.Log("jump key pressed.");
+            canJump = false;
+            Debug.Log("Player has jumnped once.");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
         }
     }
 }
